38a5d2b0adf4aa41482c9988d8b2be46a9dad6c6
Hillel Arnold <hillel.arnold@gmail.com>
2026-06-06 10:42:32 -0400

    update tests

--- test/test_update.py
+++ test/test_update.py
@@ -138,14 +138,14 @@ class UpdateTests(TestCase):
         output = self.updater.get_updated_data(refid_list, 0)
         self.assertEqual(output, [{"foo": "bar"}])
         mock_get_paged.assert_called_once_with(
-            '/repositories/2/search?q=refid:00455a772f0d2c3dde0bb2847243b2e2 OR 00455a772f0d2c3dde0bb2847243b2e3&fields[]=json&page=1')
+            '/repositories/2/search?q=refid:00455a772f0d2c3dde0bb2847243b2e2 OR 00455a772f0d2c3dde0bb2847243b2e3&type=archival_object&fields[]=json&page=1')
         mock_get_paged.reset_mock()
 
         """With last fetch time"""
         output = self.updater.get_updated_data(refid_list, 1234567)
         self.assertEqual(output, [{"foo": "bar"}])
         mock_get_paged.assert_called_once_with(
-            '/repositories/2/search?q=refid:00455a772f0d2c3dde0bb2847243b2e2 OR 00455a772f0d2c3dde0bb2847243b2e3&filter={"query": {"jsonmodel_type": "range_query", "field": "system_mtime", "from": "1970-01-15T06:56:07Z"}}&fields[]=json&page=1')
+            '/repositories/2/search?q=refid:00455a772f0d2c3dde0bb2847243b2e2 OR 00455a772f0d2c3dde0bb2847243b2e3&type=archival_object&filter={"query": {"jsonmodel_type": "range_query", "field": "system_mtime", "from": "1970-01-15T06:56:07Z"}}&fields[]=json&page=1')
 
     def test_generate_docs(self):
         category = "audio"
